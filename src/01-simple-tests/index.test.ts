598153c5507f9f7bb7526f290d3fec791f6f528b
Alena Sharai <exultantislupus@gmail.com>
2024-02-07 17:54:44 +0400

    feat: add tests for simple calculator: multiply, substract and add

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,17 +1,20 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
     // Write your test here
+    expect(simpleCalculator({ a: 2, action: Action.Add, b: 3 })).toBe(5);
   });
 
   test('should subtract two numbers', () => {
     // Write your test here
+    expect(simpleCalculator({ a: 6, b: 1, action: Action.Subtract })).toBe(5);
   });
 
   test('should multiply two numbers', () => {
     // Write your test here
+    expect(simpleCalculator({ a: 4, b: 9, action: Action.Multiply })).toBe(36);
   });
 
   test('should divide two numbers', () => {
