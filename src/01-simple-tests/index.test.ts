4aa5903b0b5687da8c1b68dbca2deb681c1fca38
Alena Sharai <exultantislupus@gmail.com>
2024-02-08 19:56:28 +0400

    feat: add tests for action in strings type and Action class type

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,50 +1,107 @@
 // Uncomment the code below and write your tests
-import { simpleCalculator } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
-    expect(simpleCalculator({ a: 2, action: '+', b: 3 })).toBe(5);
+    const resultWithString = simpleCalculator({ a: 2, b: 3, action: '+' });
+    const resultWithAction = simpleCalculator({
+      a: 4,
+      b: 7,
+      action: Action.Add,
+    });
+    expect(resultWithString).toBe(5);
+    expect(resultWithAction).toBe(11);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
-    expect(simpleCalculator({ a: 6, b: 1, action: '-' })).toBe(5);
+    const resultWithString = simpleCalculator({ a: 6, b: 1, action: '-' });
+    const resultWithAction = simpleCalculator({
+      a: 13,
+      b: 7,
+      action: Action.Subtract,
+    });
+    expect(resultWithString).toBe(5);
+    expect(resultWithAction).toBe(6);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
-    expect(simpleCalculator({ a: 4, b: 9, action: '*' })).toBe(36);
+    const resultWithString = simpleCalculator({ a: 4, b: 9, action: '*' });
+    const resultWithAction = simpleCalculator({
+      a: 11,
+      b: 7,
+      action: Action.Multiply,
+    });
+    expect(resultWithString).toBe(36);
+    expect(resultWithAction).toBe(77);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
-    expect(simpleCalculator({ a: 16, b: 4, action: '/' })).toBe(4);
-    expect(simpleCalculator({ a: 5, b: 2, action: '/' })).toBe(2.5);
+    const resultWithStringFirst = simpleCalculator({
+      a: 16,
+      b: 4,
+      action: '/',
+    });
+    const resultWithStringSecond = simpleCalculator({
+      a: 5,
+      b: 2,
+      action: '/',
+    });
+    const resultWithActionFirst = simpleCalculator({
+      a: 9,
+      b: 3,
+      action: Action.Divide,
+    });
+    const resultWithActionSecond = simpleCalculator({
+      a: 7,
+      b: 2,
+      action: Action.Divide,
+    });
+    expect(resultWithStringFirst).toBe(4);
+    expect(resultWithStringSecond).toBe(2.5);
+    expect(resultWithActionFirst).toBe(3);
+    expect(resultWithActionSecond).toBe(3.5);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
-    expect(simpleCalculator({ a: 3, b: 4, action: '^' })).toBe(81);
+    const resultWithString = simpleCalculator({ a: 3, b: 4, action: '^' });
+    const resultWithAction = simpleCalculator({
+      a: 11,
+      b: 2,
+      action: Action.Exponentiate,
+    });
+    expect(resultWithString).toBe(81);
+    expect(resultWithAction).toBe(121);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
-    expect(simpleCalculator({ a: 1, b: 2, action: '**' })).toBe(null);
+    const resultWithString = simpleCalculator({ a: 1, b: 2, action: '**' });
+    const resultWithNull = simpleCalculator({ a: 4, b: 3, action: null });
+    expect(resultWithString).toBeNull();
+    expect(resultWithNull).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
-    expect(simpleCalculator({ a: '1', b: 2, action: '+' })).toBe(null);
-    expect(simpleCalculator({ a: 1, b: '2', action: '+' })).toBe(null);
-    expect(simpleCalculator({ a: '1', b: '2', action: '+' })).toBe(null);
-    expect(simpleCalculator({ a: true, b: 2, action: '+' })).toBe(null);
-    expect(simpleCalculator({ a: 1, b: true, action: '+' })).toBe(null);
-    expect(simpleCalculator({ a: null, b: 2, action: '+' })).toBe(null);
-    expect(simpleCalculator({ a: 1, b: null, action: '+' })).toBe(null);
-    expect(simpleCalculator({ a: [1], b: 2, action: '+' })).toBe(null);
-    expect(simpleCalculator({ a: 1, b: [2], action: '+' })).toBe(null);
-    expect(simpleCalculator({ a: { 1: 1 }, b: 2, action: '+' })).toBe(null);
-    expect(simpleCalculator({ a: 1, b: { 2: 2 }, action: '+' })).toBe(null);
+    const result1 = simpleCalculator({ a: '1', b: 2, action: Action.Add });
+    const result2 = simpleCalculator({
+      a: true,
+      b: 2,
+      action: Action.Subtract,
+    });
+    const result3 = simpleCalculator({
+      a: null,
+      b: 2,
+      action: Action.Multiply,
+    });
+    const result4 = simpleCalculator({ a: [1], b: 2, action: Action.Divide });
+    const result5 = simpleCalculator({
+      a: { 1: 1 },
+      b: 2,
+      action: Action.Add,
+    });
+    expect(result1).toBeNull();
+    expect(result2).toBeNull();
+    expect(result3).toBeNull();
+    expect(result4).toBeNull();
+    expect(result5).toBeNull();
   });
 });
