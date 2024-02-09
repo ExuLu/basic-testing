52b05980fd5aa0eca42fb3a109b83d500966a13f
Alena Sharai <exultantislupus@gmail.com>
2024-02-09 13:12:48 +0400

    feat: add tests for errors in class test and add test for deposit money

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,32 +1,44 @@
 // Uncomment the code below and write your tests
-import { getBankAccount } from '.';
+import { InsufficientFundsError, TransferFailedError, getBankAccount } from '.';
 
 describe('BankAccount', () => {
-  const account = getBankAccount(300);
-
   test('should create account with initial balance', () => {
-    const result = getBankAccount(500);
-    expect(result).toEqual({ _balance: 500 });
+    const account = getBankAccount(300);
+    const balance = account.getBalance();
+    expect(account).toEqual({ _balance: 300 });
+    expect(balance).toBe(300);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
+    const account = getBankAccount(100);
     const result = () => account.withdraw(500);
-    const balance = account.getBalance();
+    expect(result).toThrow(InsufficientFundsError);
     expect(result).toThrow(
-      `Insufficient funds: cannot withdraw more than ${balance}`,
+      `Insufficient funds: cannot withdraw more than ${account.getBalance()}`,
     );
   });
 
   test('should throw error when transferring more than balance', () => {
-    // Write your test here
+    const account = getBankAccount(100);
+    const newAccount = getBankAccount(400);
+    const result = () => account.transfer(500, newAccount);
+    expect(result).toThrow(InsufficientFundsError);
+    expect(result).toThrow(
+      `Insufficient funds: cannot withdraw more than ${account.getBalance()}`,
+    );
   });
 
   test('should throw error when transferring to the same account', () => {
-    // Write your test here
+    const account = getBankAccount(300);
+    const result = () => account.transfer(200, account);
+    expect(result).toThrow(TransferFailedError);
+    expect(result).toThrow('Transfer failed');
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    const newAccount = getBankAccount(100);
+    const result = newAccount.deposit(400);
+    expect(result).toEqual({ _balance: 500 });
   });
 
   test('should withdraw money', () => {
