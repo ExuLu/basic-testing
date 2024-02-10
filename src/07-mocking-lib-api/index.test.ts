bf4bc5f3c1575d453522b0bda05635aba8f89118
Alena Sharai <exultantislupus@gmail.com>
2024-02-10 16:11:02 +0400

    feat: add add common for all tests data, mock axios module, test if create called with base url

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,10 +1,24 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
 
 describe('throttledGetDataFromApi', () => {
+  const mockAxios = axios as jest.Mocked<typeof axios>;
+  const data = [
+    { id: 1, user: 'Bob' },
+    { id: 2, user: 'Jason' },
+  ];
+  const path = './path';
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const baseURL = 'https://jsonplaceholder.typicode.com';
+    axios.get = jest.fn().mockResolvedValue(data);
+    await throttledGetDataFromApi(path);
+    jest.runAllTimers();
+    const spyCreateAxios = jest.spyOn(axios, 'create');
+    expect(spyCreateAxios).toBeCalledWith({ baseURL: baseURL });
   });
 
   test('should perform request to correct provided url', async () => {
